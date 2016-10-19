90763335f38e92e0205b1d63662b03065f35b971
PeaceOff <davidazevedo1996@hotmail.com>
2016-10-19 07:59:44 +0100

    Patched cylinder to better align with z axis and fixed firstScene.xml

--- reader/Cylinder.js
+++ reader/Cylinder.js
@@ -1,7 +1,7 @@
  function Cylinder(scene, base, top, height, slices, stacks) {
- 	
+
  	CGFobject.call(this,scene);
-	
+
 	this.base = base;
 	this.top = top;
 	this.height = height;
@@ -9,7 +9,7 @@
 	this.stacks = stacks;
 	var angulo = 360 / this.slices;
 	this.aRad = (angulo * Math.PI) / 180;
-	
+
  	this.initBuffers();
  };
 
@@ -18,46 +18,46 @@
 
 
  Cylinder.prototype.createCircle = function(radius,z,isTop){
-   
+
     var ang = 0;
-    
+
  	var indiceTemp = this.vertices.length/3;
 
 	for(var i = 0 ; i < this.slices; i++){
 
         var x = radius * Math.cos(ang);
-        var y = radius * Math.sin(ang); 
+        var y = radius * Math.sin(ang);
 		var xx = Math.cos(ang);
 		var yy = Math.sin(ang);
 
-        this.vertices.push(x,z,y);
-        
+        this.vertices.push(x,y,z);
+
         if(isTop == 0){
-            this.normals.push(0,-1,0);
+            this.normals.push(0,0,-1);
         } else {
-            this.normals.push(0,1,0);
+            this.normals.push(0,0,1);
         }
 
         if(i < this.slices - 2 )
         {
           	  if(isTop == 0){
-                this.indices.push(indiceTemp, indiceTemp + i + 1, indiceTemp + i + 2);
+                this.indices.push(indiceTemp, indiceTemp + i + 2 , indiceTemp + i + 1);
             } else {
-                this.indices.push(indiceTemp, indiceTemp + i + 2, indiceTemp + i + 1);
+                this.indices.push(indiceTemp, indiceTemp + i + 1, indiceTemp + i + 2);
             }
         }
-        var s = (xx+1) /2.0; 
+        var s = (xx+1) /2.0;
 
-        var v = ((yy*-1)+1) /2.0; 
+        var v = ((yy*-1)+1) /2.0;
 
-        this.texCoords.push( s, v); 
+        this.texCoords.push( s, v);
 
         ang += this.aRad;
     }
  }
 
 Cylinder.prototype.calculateCross = function(A,B){
-	
+
 	var cross = {
 		x: A.y * B.z - A.z * B.y,
 		y: A.z * B.x - A.x - B.z,
@@ -73,17 +73,17 @@ Cylinder.prototype.calculateCross = function(A,B){
 }
 
 Cylinder.prototype.calculateNormal = function(angle){
-	
+
 	var tangent ={
-		x: Math.sin(angle) , 
-		y: 0,
-		z: -Math.cos(angle)
+		x: Math.sin(angle) ,
+		y: -Math.cos(angle),
+		z: 0
 		};
-	
+
 	var vetor = {
 		x: (this.top - this.base) * Math.cos(angle) ,
-		y: this.height, 
-		z: (this.top - this.base) * Math.sin(angle)
+		y: (this.top - this.base) * Math.sin(angle),
+		z: this.height
 		};
 	return this.calculateCross(tangent,vetor);
 }
@@ -95,9 +95,9 @@ Cylinder.prototype.calculateNormal = function(angle){
  	this.indices = [];
  	this.normals = [];
  	this.texCoords = [];
-	
-	
-    
+
+
+
     for(var i = 0; i <= this.stacks; i++){
 		var h = i/this.stacks;
 
@@ -107,9 +107,9 @@ Cylinder.prototype.calculateNormal = function(angle){
 
 			var radius = (this.top-this.base)* h + this.base;
 			var x = radius * Math.cos(ang);
-			var z = radius * Math.sin(ang);
-			
-			this.vertices.push(x,this.height * h, z);
+			var y = radius * Math.sin(ang);
+
+			this.vertices.push(x , y , this.height * h);
 			var normal = this.calculateNormal(ang);
 			this.normals.push(normal.x,normal.y,normal.z);
 
@@ -118,7 +118,7 @@ Cylinder.prototype.calculateNormal = function(angle){
 			this.texCoords.push(s,1-h);
     	}
     }
-	
+
 
 	var incr1 = this.slices+1;
 
@@ -126,13 +126,13 @@ Cylinder.prototype.calculateNormal = function(angle){
 		for(var k = 0 ; k < this.slices; k++){
 			var index = i * (incr1) + k;
 
-			this.indices.push(index + 1, index, index + incr1);
-			this.indices.push( index + incr1 + 1 , index + 1, index + incr1);
+			this.indices.push(index + 1, index + incr1, index);
+			this.indices.push( index + incr1 + 1 , index + incr1, index + 1);
 		}
-		
+
 
 	}
-	
+
 
 	this.createCircle(this.base,0,0);
 	this.createCircle(this.top,this.height,1);
